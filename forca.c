3633c3c4624fe101d1f7842556b01697b458df8d
valn3y <neymarinho.junior@gmail.com>
2022-09-25 14:55:35 -0400

    clean code

--- forca.c
+++ forca.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-	char secretWord[20];
-	sprintf(secretWord, "MELANCIA");
+char secretWord[20];
+char kicks[26];
+int tries = 0;
 
+void opening() {
+	printf("*************************\n");
+	printf("*     Jogo da Forca     *\n");
+	printf("*************************\n\n");
+}
+
+void actionGuess() {
+	char guess;
+	scanf(" %c", &guess);
+
+	kicks[tries] = guess;
+	tries++;
+}
+
+int alreadyGuess(char letter) {
+	int found = 0;
+	for(int j = 0; j < tries; j++) {
+		if(kicks[j] == letter) {
+			found = 1;
+			break;
+		}
+	}
+
+	return found;
+}
+
+void drawGallows() {
+	// start the game!!!
+	for(int i = 0; i < strlen(secretWord); i++) {
+		int found = alreadyGuess(secretWord[i]);
+		if(found) {
+			printf("%c ", secretWord[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+	printf("\n");
+}
+
+void choseWord() {
+	sprintf(secretWord, "MELANCIA");
+}
+
+int main() {
 	int correct = 0;
 	int hanged = 0;
 
-	char kicks[26];
-	int tries = 0;
+	choseWord();
+	opening();
 
 	do {
-		// start the game!!!
-		for(int i = 0; i < strlen(secretWord); i++) {
-			int found = 0;
-			for(int j = 0; j < tries; j++) {
-				if(kicks[j] == secretWord[i]) {
-					found = 1;
-					break;
-				}
-			}
-
-			if(found) {
-				printf("%c ", secretWord[i]);
-			} else {
-				printf("_ ");
-			}
-		}
-		printf("\n");
-
-		char guess;
-		scanf(" %c", &guess);
-
-		kicks[tries] = guess;
-		tries++;
+		drawGallows();
+		actionGuess();
 
 	} while(!correct && !hanged);
 }
